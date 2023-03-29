0ccce4f56b1e7fd11f969f2f8d8242657bcbed0f
Atul <patelatulpatel@gmail.com>
2023-03-30 02:10:53 +0530

    FIXED

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const router = express.Router();
+
+
 const chatsController = require('../controllers/chatsController');
 const imageController = require("../controllers/imageController");
 const authenticate = require('../middleware/auth');
-//const { multerMiddleware } = require("../middleware/multer.js")
 
-const router = express.Router();
 
 router.get('/chats/getAllChats', authenticate.authenticate, chatsController.getAllChats);
 router.post('/chats/saveChatMsg', authenticate.authenticate, chatsController.postChat);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
-const usersController = require('../controllers/usersController');
-
-const authenticate=require('../middleware/auth')
-
 const router = express.Router();
 
-router.post('/users/signup', usersController.createUser)
+const usersController = require('../controllers/usersController');
+const authenticate = require('../middleware/auth');
 
-router.post('/users/signin', usersController.signUser)
 
-router.get('/users/getAllUsers', authenticate.authenticate, usersController.getUsers)
+router.post('/users/signup', usersController.createUser);
+router.post('/users/signin', usersController.signUser);
+router.get('/users/getAllUsers', authenticate.authenticate, usersController.getUsers);
 
-module.exports=router;
+
+
+module.exports = router;
